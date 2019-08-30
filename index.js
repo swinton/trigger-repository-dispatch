2b0ff8b9654efd0ef01498d5bab81d8511f53a5d
Steve Winton <stevewinton@gmail.com>
2019-08-30 14:04:43 -0500

    Trigger repository dispatch

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+const Octokit = require('@octokit/rest');
+const argv = require('minimist')(process.argv.slice(2));
 const logger = require('./lib/logger');
 
-const items = [1, 2, 3, 4, 5];
+const mediaType = 'application/vnd.github.everest-preview+json';
+const headers = { accept: mediaType };
 
-const sum = items.reduce((total, item) => total + item, 0);
+(async () => {
+  try {
+    // We need a nwo parameter...
+    if (!argv.nwo) {
+      throw new Error('Required: name with owner (--nwo) parameter');
+    }
 
-logger.info(sum);
+    // Construct client
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+    // Send a repository dispatch event
+    octokit.request({
+      method: 'POST',
+      url: `https://api.github.com/repos/${argv.nwo}/dispatches`,
+      headers,
+      data: {
+        event_type: argv['event-type'] || 'trigger-repository-dispatch'
+      }
+    });
+  } catch (e) {
+    logger.error(e);
+  }
+})();
